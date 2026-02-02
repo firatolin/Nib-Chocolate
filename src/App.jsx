f1613d144fdd2f988cee706596a522bdab3e1bc7
Firatol Esayas <teferafiratolesayas@gmail.com>
2026-02-02 18:35:20 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SmoothScroll from "./SmoothScroll";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Taste from "./components/Taste";
+import Quality from "./components/Quality";
+import Parallax from "./components/Parallax";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <SmoothScroll>
+      <Header />
+      <Hero />
+      <Taste />
+      <Quality />
+      <Parallax />
+      <Footer />
+    </SmoothScroll>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="sticky top-0 z-20 flex items-stretch justify-between gap-5 max-w-7xl m-auto p-6">
+        <div clasName="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3"></div>
+      </header>
+    </>
+  );
+};
+export default Header;
