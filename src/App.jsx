3cb3cd389864f99c77dacfa05aa070dc88e493f9
Firatol Esayas <teferafiratolesayas@gmail.com>
2026-02-04 19:04:40 +0300

    header logo updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import SmoothScroll from "./SmoothScroll";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Taste from "./components/Taste";
-import Quality from "./components/Quality";
-import Parallax from "./components/Parallax";
-import Footer from "./components/Footer";
+// import Hero from "./components/Hero";
+// import Taste from "./components/Taste";
+// import Quality from "./components/Quality";
+// import Parallax from "./components/Parallax";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
     <SmoothScroll>
       <Header />
-      <Hero />
-      <Taste />
-      <Quality />
-      <Parallax />
-      <Footer />
     </SmoothScroll>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <>
       <header className="sticky top-0 z-20 flex items-stretch justify-between gap-5 max-w-7xl m-auto p-6">
-        <div clasName="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3"></div>
+        <div clasName="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3">
+          <img src={Logo} alt="" className="max-w-36" />
+          <nav>
+            <ul className="flex items-center gap-9 font-medium tracking-tight">
+              <li>
+                <a href="">Flavors</a>
+              </li>
+              <li>
+                <a href="">Our Story</a>
+              </li>
+              <li>
+                <a href="">How it's Made</a>
+              </li>
+              <li>
+                <a href="">Gifting</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </>
   );
