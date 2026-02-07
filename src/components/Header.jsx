3fd99dae5a050af9c7e733575f911aa5fdf4a878
Firatol Esayas <teferafiratolesayas@gmail.com>
2026-02-07 22:31:09 +0300

    Header.jsx typo debugged

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <header className="sticky top-0 z-20 flex items-stretch justify-between gap-5 max-w-7xl m-auto p-6">
-        <div clasName="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3">
+        <div className="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3">
           <img src={Logo} alt="" className="max-w-36" />
           <nav>
             <ul className="flex items-center gap-9 font-medium tracking-tight">
